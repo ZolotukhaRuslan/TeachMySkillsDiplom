af66f583ae206adf4ed03d56f5866c0dca1a7731
Ruslan <Zolotukharv@gmail.com>
2022-07-29 10:57:35 +0300

    localization enabled

--- src/main/java/com/example/diplommaket/config/InternationalizationConfig.java
+++ src/main/java/com/example/diplommaket/config/InternationalizationConfig.java
@@ -1,11 +1,17 @@
 package com.example.diplommaket.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,15 +19,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+
 @Configuration
 @EnableWebMvc
 public class InternationalizationConfig implements WebMvcConfigurer {
     @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        registrationBean.setFilter(characterEncodingFilter);
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        registrationBean.setOrder(Integer.MIN_VALUE);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    @Bean("messageSource")
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
         source.setBasename("classpath:locale/message");
         source.setDefaultEncoding("UTF-8");
-        source.setUseCodeAsDefaultMessage(true);
         return source;
     }
 
@@ -30,6 +52,7 @@ public class InternationalizationConfig implements WebMvcConfigurer {
         CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
         return cookieLocaleResolver;
     }
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
